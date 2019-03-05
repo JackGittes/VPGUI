970750be5cb8c48e88740dae4579ce737e8699bf
Zhao Mingxin <1554146770@qq.com>
2019-03-05 14:41:09 +0800

    add changes

--- RunButton.py
+++ RunButton.py
@@ -2,23 +2,27 @@ from PyQt5.QtWidgets import QApplication,QLabel
 from PyQt5.QtWidgets import QWidget,QFileDialog,QMainWindow
 from PyQt5 import QtWidgets,QtCore
 from PyQt5.QtGui import *
-import sys
+import sys,os
+import utils
+import warnings
 
 class RunButton(QWidget):
     def __init__(self):
         super().__init__()
-        vLayout = QtWidgets.QVBoxLayout()
+        self.SetupButton()
 
+    def SetupButton(self):
+        vLayout = QtWidgets.QVBoxLayout()
         self.ExitButton = QtWidgets.QPushButton("结束并关闭")
         self.ExitButton.setIcon(QIcon("./icons/png/close.png"))
 
         self.LoadFileButton = QtWidgets.QPushButton("加载数据")
         self.LoadFileButton.setIcon(QIcon("./icons/png/openfile.png"))
 
-        self.DownloadParamsButton = QtWidgets.QPushButton("下载数据",self)
+        self.DownloadParamsButton = QtWidgets.QPushButton("下载数据")
         self.DownloadParamsButton.setIcon(QIcon("./icons/png/down.png"))
 
-        self.StartTestButton = QtWidgets.QPushButton("开始测试",self)
+        self.StartTestButton = QtWidgets.QPushButton("开始测试")
         self.StartTestButton.setIcon(QIcon("./icons/png/start.png"))
 
         vLayout.addWidget(self.LoadFileButton)
@@ -26,6 +30,9 @@ class RunButton(QWidget):
         vLayout.addWidget(self.StartTestButton)
         vLayout.addWidget(self.ExitButton)
         self.setLayout(vLayout)
+
+    def __del__(self):
+        print("Run Button Destroyed.")
 
 class LogTab(QWidget):
     def __init__(self):
@@ -40,44 +47,76 @@ class LogTab(QWidget):
 
         self.setLayout(hLayout)
 
-class ResultArea(QWidget):
+class TestInfoBox(QWidget):
     def __init__(self):
         super().__init__()
         widget = QtWidgets.QWidget()
 
-        hwidget = QtWidgets.QWidget()
-        vwidget = QtWidgets.QWidget()
+        self.TotalInfo = QtWidgets.QLabel(widget)
+        self.TotalNumber = QtWidgets.QLCDNumber(widget)
+        self.TestInfo = QtWidgets.QLabel(widget)
+        self.FPS = QtWidgets.QLCDNumber(widget)
 
-        hLayout = QtWidgets.QHBoxLayout()
+        self.TotalInfo.setText("总测试数量(张)")
+        self.TestInfo.setText("当前帧率(FPS)")
         vLayout = QtWidgets.QVBoxLayout()
+        vLayout.addWidget(self.TotalInfo)
+        vLayout.addWidget(self.TotalNumber)
+        vLayout.addWidget(self.TestInfo)
+        vLayout.addWidget(self.FPS)
+        self.setLayout(vLayout)
+
+class ResultArea(QWidget):
+    def __init__(self):
+        super().__init__()
+        h1widget = QtWidgets.QWidget()
+        h2widget = QtWidgets.QWidget()
+
+        gLayout = QtWidgets.QVBoxLayout()
+        h1Layout = QtWidgets.QHBoxLayout()
+        h2Layout = QtWidgets.QHBoxLayout()
 
         """Two Images to Display"""
-        self.Origin = QLabel(widget)
-        self.Tested = QLabel(widget)
+        self.Origin = QLabel(h1widget)
+        self.Tested = QLabel(h1widget)
 
         """A display tool used to control image display mode"""
-
         self.Origin.setPixmap(QPixmap("./imgs/origin.png"))
         self.Tested.setPixmap(QPixmap("./imgs/tested.png"))
 
-        hLayout.addWidget(self.Origin)
-        hLayout.addWidget(self.Tested)
+        h1Layout.addWidget(self.Origin)
+        h1Layout.addWidget(self.Tested)
 
-        self.PreviousButton = QtWidgets.QToolButton(widget)
-        self.PlayPauseButton = QtWidgets.QToolButton(widget)
-        self.NextButton = QtWidgets.QToolButton(widget)
+        self.PreviousButton = QtWidgets.QPushButton(h2widget)
+        self.PlayPauseButton = QtWidgets.QPushButton(h2widget)
+        self.NextButton = QtWidgets.QPushButton(h2widget)
+        self.Progress = QtWidgets.QProgressBar(h2widget)
+        self.Progress.setMinimum(1)
+        self.Progress.setMaximum(1000)
+
         self.SetButtonNames()
 
-        vLayout.addWidget(self.PreviousButton)
-        vLayout.addWidget(self.PlayPauseButton)
-        vLayout.addWidget(self.NextButton)
+        h2Layout.addWidget(self.PreviousButton)
+        h2Layout.addWidget(self.PlayPauseButton)
+        h2Layout.addWidget(self.NextButton)
+        h2Layout.addWidget(self.Progress)
 
-        self.setLayout(hLayout)
+        h1widget.setLayout(h1Layout)
+        h2widget.setLayout(h2Layout)
+
+        gLayout.addWidget(h1widget)
+        gLayout.addWidget(h2widget)
+
+        self.setLayout(gLayout)
 
     def SetButtonNames(self):
         self.PreviousButton.setText("上一张")
         self.PlayPauseButton.setText("暂停/播放")
         self.NextButton.setText("下一张")
+
+        self.PreviousButton.setIcon(QIcon("./icons/png/previous_one.png"))
+        self.PlayPauseButton.setIcon(QIcon("./icons/png/play.png"))
+        self.NextButton.setIcon(QIcon("./icons/png/next_one.png"))
 
 class StatusBottom(QWidget):
     def __init__(self):
@@ -86,10 +125,34 @@ class StatusBottom(QWidget):
 """ VPGUI Mainwindow used for vision chip test platform"""
 
 class VPGUI(QMainWindow):
-    def __init__(self):
+    def __init__(self,width=1500,height=900):
         super().__init__()
-        self.Width = 1200
-        self.Height = 800
+        if width<800 or height<600:
+            width_warn = "Window width shouldn't be less than 800\n"
+            height_warn = "Window height shouldn't be less than 600\n"
+            warnings.warn(width_warn+height_warn)
+            width = 800
+            height = 600
+
+        self.Width = width
+        self.Height = height
+
+        self.Version = 1.0
+        self.Name = "视觉芯片软件测试平台"
+        self.setWindowTitle(self.Name+str(self.Version))
+
+        self.SetupGUI()
+        self.BuildConnect()
+
+    def BuildConnect(self):
+        self.RunCtrl.ExitButton.clicked.connect(lambda: ExitMessageBox(self))
+        self.RunCtrl.LoadFileButton.clicked.connect(lambda: self.BrowseFile())
+        # self.RunCtrl.DownloadParamsButton.click.connect()
+        # self.RunCtrl.StartTestButton.clicked.connect()
+        #
+        # self.ResultImg.PreviousButton.clicked.connect()
+        # self.ResultImg.PlayPauseButton.clicked.connect()
+        # self.ResultImg.NextButton.clicked.connect()
 
     def SetupGUI(self):
         width = self.Width
@@ -101,23 +164,27 @@ class VPGUI(QMainWindow):
         self.SetupMenuBar()
         self.SetupToolBar()
 
-        central_widget = QtWidgets.QWidget()
+        self.gwidget = QtWidgets.QWidget()
+
+        self.ResultImg = ResultArea()
+        self.LogArea = LogTab()
+        self.TestArea = TestInfoBox()
+        self.RunCtrl = RunButton()
 
         bottomWidget = QtWidgets.QWidget()
 
         hLayout = QtWidgets.QHBoxLayout()
-        hLayout.addWidget(LogTab())
-        hLayout.addWidget(RunButton())
+        hLayout.addWidget(self.LogArea)
+        hLayout.addWidget(self.TestArea)
+        hLayout.addWidget(self.RunCtrl)
         bottomWidget.setLayout(hLayout)
 
         vLayout = QtWidgets.QVBoxLayout()
-        vLayout.addWidget(ResultArea())
+        vLayout.addWidget(self.ResultImg)
         vLayout.addWidget(bottomWidget)
 
-        central_widget.setLayout(vLayout)
-
-        self.setCentralWidget(central_widget)
-
+        self.gwidget.setLayout(vLayout)
+        self.setCentralWidget(self.gwidget)
         return True
 
     def SetupMenuBar(self):
@@ -127,7 +194,8 @@ class VPGUI(QMainWindow):
         RunMenu = self.menuBar().addMenu("运行")
         HelpMenu = self.menuBar().addMenu("帮助")
 
-        PathAct = FileMenu.addAction('工作路径')
+        OpenFile = FileMenu.addAction("打开")
+        PathAct = FileMenu.addAction("路径")
         PreferenceAct = FileMenu.addAction("偏好")
         ExitAct = FileMenu.addAction("退出")
 
@@ -146,12 +214,14 @@ class VPGUI(QMainWindow):
         TestModeButton = QtWidgets.QPushButton("测试模式")
         PrepareButton = QtWidgets.QPushButton("初始化")
         PreprocessButton = QtWidgets.QPushButton("预处理")
-        ExitTestButton = QtWidgets.QPushButton("退出测试")
         UploadButton = QtWidgets.QPushButton("返回结果")
+        ResetButton = QtWidgets.QPushButton("复位状态")
+        ExitTestButton = QtWidgets.QPushButton("退出测试")
         TestModeButton.setIcon(QIcon("./icons/png/settings.png"))
         ExitTestButton.setIcon(QIcon("./icons/png/stop.png"))
         PrepareButton.setIcon(QIcon("./icons/png/connect.png"))
         PreprocessButton.setIcon(QIcon("./icons/png/process.png"))
+        ResetButton.setIcon(QIcon("./icons/png/reset_hint.png"))
         UploadButton.setIcon(QIcon("./icons/png/upload.png"))
 
         self.toolbar = self.addToolBar("快捷设置")
@@ -160,6 +230,7 @@ class VPGUI(QMainWindow):
         self.toolbar.addWidget(PrepareButton)
         self.toolbar.addWidget(PreprocessButton)
         self.toolbar.addWidget(UploadButton)
+        self.toolbar.addWidget(ResetButton)
         self.toolbar.addWidget(ExitTestButton)
 
         TestMode = QtWidgets.QMenu()
@@ -171,10 +242,52 @@ class VPGUI(QMainWindow):
         TestModeButton.setMenu(TestMode)
         TestMode.triggered.connect(lambda action: print(action.text()))
 
+    def BrowseFile(self, path=None):
+        if path == None:
+            path = os.getcwd()
+        filename = QFileDialog.getOpenFileName(self, "选择文件", path)
+        print(filename[0])
+        return filename
+
+    def DownloadParams(self):
+        ChooseLoadType = QtWidgets.QMessageBox()
+        ChooseLoadType.setWindowTitle("选择下载位置")
+        ChooseLoadType.setGeometry(QtCore.QRect(self.Width/2-100,self.Height/2-50,200,100))
+        ChooseLoadType.setText("请选择参数下载位置：")
+
+        mpuButton = ChooseLoadType.addButton("MPU",QtWidgets.QMessageBox.YesRole)
+        vpButton = ChooseLoadType.addButton("VP",QtWidgets.QMessageBox.YesRole)
+        ChooseLoadType.exec_()
+
+        if ChooseLoadType.clickedButton()==mpuButton:
+            ChooseLoadType.sender()
+        elif ChooseLoadType.clickedButton()==vpButton:
+            self.RunStatusMessage.append("正在写入VP")
+        else:
+            ChooseLoadType.destroy()
+
+def ExitMessageBox(self):
+    Msg = QtWidgets.QMessageBox()
+    Msg.setGeometry(QtCore.QRect(0, 0, 100, 40))
+    IconMap = QPixmap("./icons/png/unlink.png")
+    IconMap = IconMap.scaled(60, 60)
+    Msg.setIconPixmap(IconMap)
+    Msg.setText("确认要退出？")
+    Msg.setInformativeText("未保存的信息将丢失")
+    Msg.setWindowTitle("提示")
+    yesButton = Msg.addButton("确定", QtWidgets.QMessageBox.YesRole)
+    Msg.addButton("取消", QtWidgets.QMessageBox.NoRole)
+    """Set Messsage Window to Desktop Center"""
+    qtRectangle = Msg.frameGeometry()
+    centerPoint = QtWidgets.QDesktopWidget().availableGeometry().center()
+    qtRectangle.moveCenter(centerPoint)
+    Msg.move(qtRectangle.topLeft())
+    Msg.exec_()
+    if Msg.clickedButton() == yesButton:
+        self.close()
 
 if __name__=="__main__":
     app = QApplication(sys.argv)
     button = VPGUI()
-    button.SetupGUI()
     button.show()
     sys.exit(app.exec_())
---
+++ utils.py
@@ -0,0 +1,38 @@
+import cv2
+import
+
+class Preprocess(object):
+    def __init__(self,parent=None):
+        super().__init__()
+    #todo:提供至少三种图像预处理的函数，并且一个统一入口，根据设置的类型选择具体的预处理算法
+
+
+def ImgResize(img,width=None,height=None):
+    """不要改变图片的比例，最终图片至少有一个边，充满"""
+
+    return rgbimg
+
+def Img2Bin(path):
+
+    return binary_file,imglist
+
+class DisplayPanel():
+    def __init__(self,maxmimum=None):
+        self.MAX = maxmimum
+        self.index = 0
+
+    def ImgContinuousDisplay(list,time_interval=0.3):
+
+        return rgb
+
+    def ImgSingleDisplay(self):
+        return rgb
+
+    def PauseAndPause(self):
+        return rgb
+
+
+def LogWriter(path):
+    #todo:
+
+    return None
